3d0588ed167ab40aafbdc006efbb067aca343423
Dmitri Shevchenko <dmitri.shevchenko.au@gmail.com>
2019-11-02 13:33:52 +1100

    Commit

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-please work, i am beging you i just want to commit why wont you commit?
-will commit? now? PLease?
+import pyowm
+owm = pyowm.OWM("85b7235ca94da8d941ddfc41f9cd7e51")
+observation = owm.weather_at_place("North Sydney, AU")
+
+def getWeather():
+    weather = observation.get_weather()
+    temperature = weather.get_temperature('celsius')['temp']
+    humidity = weather.get_humidity()
+    cloudCover = weather.get_clouds() 
+    rain = weather.get_rain()
+    wind = weather.get_wind()['speed']
+    sunrise = weather.get_sunrise_time(timeformat='iso')
+    sunset = weather.get_sunset_time(timeformat='iso')
+    CurrentWeatherDict = {
+        "temp": temperature,
+        "wind": wind,
+        "rain": rain,
+        "humi": humidity,
+        "covr": cloudCover,
+        "sset": sunset,
+        "sris": sunrise
+    }
+    return CurrentWeatherDict
+
+def getForcast():
+    fc = owm.three_hours_forecast('North Sydney, AU')
+    f = fc.get_forecast()
+    lst = f.get_weathers()
+    for weather in f:
+        print (weather.get_reference_time('iso'),weather.get_clouds())
+
+print(getWeather()['humi'])
